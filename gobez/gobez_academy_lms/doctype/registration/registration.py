fd54dcd9e7bcfb85ca32a2dad0ad53a7740466c5
Ahmed <hmdzabu@gmail.com>
2025-08-02 22:32:18 +0000

    new commit

---
+++ gobez/gobez_academy_lms/doctype/registration/registration.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Ahmed Salim and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Registration(Document):
+	pass
