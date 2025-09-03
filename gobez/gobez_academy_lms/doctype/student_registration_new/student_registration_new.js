908c13a192a21ea8d0146abb3c23b7c6e71b4e37
Ahmed <hmdzabu@gmail.com>
2025-09-03 04:36:10 +0000

    sdfa

---
+++ gobez/gobez_academy_lms/doctype/student_registration_new/student_registration_new.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ahmed Salim and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Student Registration new", {
+// 	refresh(frm) {
+
+// 	},
+// });
